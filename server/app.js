338b179a431f3cfdb9ddd00d606a5681f59952cb
antaresofficial <antaresofficial@icloud.com>
2022-03-13 02:02:44 +0500

    init

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+
+const app = express()
+const PORT = 8080
+
+app.use('/graphql', graphqlHTTP({}))
+
+app.listen(PORT, err => {
+  err ? console.log(err) : console.log(`Server was started in ${PORT}`)
+})
